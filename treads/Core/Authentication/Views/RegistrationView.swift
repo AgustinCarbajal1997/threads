e9b8991b08b5180d675d2301abe07b385003a991
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-19 17:10:03 -0300

    [feature] modifiers

--- treads/Core/Authentication/Views/RegistrationView.swift
+++ treads/Core/Authentication/Views/RegistrationView.swift
@@ -8,8 +8,62 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var fullName: String = ""
+    @State private var username: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Image("app-icon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 120)
+                .padding()
+            
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .autocapitalization(.none)
+                    .modifier(ThreadsTextFieldModifiers())
+                SecureField("Enter your password", text: $password)
+                    .modifier(ThreadsTextFieldModifiers())
+                
+                TextField("Enter your full name", text: $fullName)
+                    .modifier(ThreadsTextFieldModifiers())
+                
+                TextField("Enter your username", text: $username)
+                    .modifier(ThreadsTextFieldModifiers())
+            }
+            
+            Button{} label: {
+                Text("Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 352, height: 44)
+                    .background(.black)
+                    .cornerRadius(8)
+                    
+            }
+            .padding(.vertical)
+            
+            Spacer()
+            
+            Divider()
+            
+            Button{}label: {
+                HStack(spacing: 3){
+                    Text("Already have an account?")
+                    Text("Sign In")
+                        .fontWeight(.semibold)
+                }
+                .font(.footnote)
+            }
+            .foregroundColor(.black)
+            .padding(.vertical, 16)
+        }
     }
 }
 
--- treads/Utils/ViewModifiers/ThreadsFieldTextModifiers.swift
+++ treads/Utils/ViewModifiers/ThreadsFieldTextModifiers.swift
@@ -5,4 +5,16 @@
 //  Created by Agustin Carbajal on 19/11/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct ThreadsTextFieldModifiers: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal, 24)
+    }
+    
+}
