ed4e7b81a4df255416919da9d08629a9d61f9100
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-19 16:26:44 -0300

    Initial Commit

---
+++ treads/treadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  treadsApp.swift
+//  treads
+//
+//  Created by Agustin Carbajal on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct treadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
