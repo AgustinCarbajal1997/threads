fc7148b93b02bf1ff166df369a40417f2e5a5d80
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-20 18:39:21 -0300

    [feature] user cells

---
+++ treads/Core/Components/CircularProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularProfileView.swift
+//  treads
+//
+//  Created by Agustin Carbajal on 20/11/2023.
+//
+
+import SwiftUI
+
+struct CircularProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircularProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProfileView()
+    }
+}
---
+++ treads/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,20 @@
+//
+//  ThreadCell.swift
+//  treads
+//
+//  Created by Agustin Carbajal on 20/11/2023.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThreadCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ThreadCell()
+    }
+}
