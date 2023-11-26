0478a04fa98988d12e9f704a44c32ec1273eead2
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-25 21:21:19 -0300

    [feature] profile view model & add firebase

---
+++ treads/Core/Root/ViewModel/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  treads
+//
+//  Created by Agustin Carbajal on 25/11/2023.
+//
+
+import Foundation
---
+++ treads/Services/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  treads
+//
+//  Created by Agustin Carbajal on 25/11/2023.
+//
+
+import Foundation
