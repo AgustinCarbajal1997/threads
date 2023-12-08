885fed39b69ac6575fbc9e40fd5562d452ed9e98
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-12-08 13:08:52 -0300

    [feature] threads

--- treads/Model/Thread.swift
+++ treads/Model/Thread.swift
@@ -5,4 +5,20 @@
 //  Created by Agustin Carbajal on 08/12/2023.
 //
 
-import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Thread: Identifiable, Codable {
+    @DocumentID var threadId: String?
+    
+    let ownerUid: String
+    let caption: String
+    let timestamp: Timestamp
+    var likes: Int
+    
+    var id: String {
+        return threadId ?? NSUUID().uuidString
+    }
+    
+    var user: User?
+}
